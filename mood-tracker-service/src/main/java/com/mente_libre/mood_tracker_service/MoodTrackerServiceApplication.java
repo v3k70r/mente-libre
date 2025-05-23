c5a04f2d10dc4b01cfc0c85308539bd7b0d7e874
SebaRojo24 <seba.rojo@duocuc.cl>
2025-05-22 21:41:59 -0400

    Se inicia mood-tracker-service.

---
+++ mood-tracker-service/src/main/java/com/mente_libre/mood_tracker_service/MoodTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mente_libre.mood_tracker_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodTrackerServiceApplication.class, args);
+	}
+
+}
---
+++ mood-tracker-service/src/test/java/com/mente_libre/mood_tracker_service/MoodTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mente_libre.mood_tracker_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodTrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
