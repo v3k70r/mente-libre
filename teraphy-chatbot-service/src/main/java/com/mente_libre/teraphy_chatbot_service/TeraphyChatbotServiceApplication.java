53f075b615b41960de104afaa7659a1702073a9b
Víctor Albarrán <victor.a.albarran.n@gmail.com>
2025-05-22 21:05:20 -0400

    Se inicia chatbot.

---
+++ teraphy-chatbot-service/src/main/java/com/mente_libre/teraphy_chatbot_service/TeraphyChatbotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mente_libre.teraphy_chatbot_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeraphyChatbotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeraphyChatbotServiceApplication.class, args);
+	}
+
+}
