5139210e73ecfe6b6090aebe7e333de842759e5b
Víctor Albarrán <victor.a.albarran.n@gmail.com>
2025-05-22 21:38:06 -0400

    Se inicia notification-service.

---
+++ notification-service/src/test/java/com/mente_libre/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mente_libre.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
